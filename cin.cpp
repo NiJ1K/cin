9fea6ef2d4ab27bbefec1f34aa57fb88d5b53da2
Andrew Nizhnikovski <119796680+NiJ1K@users.noreply.github.com>
2023-04-25 22:31:14 +0300

    Добавьте файлы проекта.

---
+++ cin.cpp
@@ -0,0 +1,125 @@
+﻿//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    double num1, num2, num3, average;
+//    cout << "Enter three numbers: ";
+//    cin >> num1 >> num2 >> num3;
+//    average = (num1 + num2 + num3) / 3.0;
+//    cout << "The average is " << average << endl;
+//    return 0;
+//}#include <iostream>
+
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    double a, b, x;
+//    cout << "Enter the values of a and b in ax + b = 0: ";
+//    cin >> a >> b;
+//    x = -b / a;
+//    cout << "The solution is x = " << x << endl;
+//    return 0;
+//}
+
+
+
+
+//#include <iostream>
+//#include <cmath>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    double num, exponent, result;
+//    cout << "Enter a number: ";
+//    cin >> num;
+//    cout << "Enter the exponent: ";
+//    cin >> exponent;
+//    result = pow(num, exponent);
+//    cout <<  result << endl;
+//    return 0; 
+//}
+
+
+//
+//#include <iostream>
+//
+//using namespace std;
+//
+//const double PI = 3.14159;
+//
+//int main()
+//{
+//    double radius, area, circumference;
+//    cout << "Enter the radius of the circle: ";
+//    cin >> radius;
+//    area = PI * radius * radius;
+//    circumference = 2 * PI * radius;
+//    cout << "The area of the circle is " << area << endl;
+//    cout << "The circumference of the circle is " << circumference << endl;
+//    return 0;
+//}
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    const double USD_RATE = 0.025; 
+//    const double EUR_RATE = 0.023; 
+//    const double BTC_RATE = 0.00000090; 
+//    double uah, usd, eur, btc;
+//    cout << "Enter the amount of UAH: ";
+//    cin >> uah;
+//    usd = uah * USD_RATE;
+//    eur = uah * EUR_RATE;
+//    btc = uah * BTC_RATE;
+//    cout << "UAH " << uah << " is equivalent to " << usd << " USD, " << eur << " EUR, and " << btc << " BTC." << endl;
+//    return 0;
+//}
+
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    const double KILOMETERS_IN_MILE_LAND = 1.609; 
+//    const double KILOMETERS_IN_MILE_SEA = 1.852; 
+//
+//    double kilometers;
+//
+//    cout << "Enter kilometers: ";
+//    cin >> kilometers;
+//
+//    
+//    cout << kilometers << " kilometers is equal to " << kilometers / KILOMETERS_IN_MILE_LAND << " land miles or " << kilometers / KILOMETERS_IN_MILE_SEA << " sea miles." << endl;
+//
+//    return 0;
+//}
+
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    double n, p, result;
+//    cout << "Enter a number: ";
+//    cin >> n;
+//    cout << "Enter a percentage: ";
+//    cin >> p;
+//    result = (p / 100) * n;
+//    cout << p << "% of " << n << " is " << result << endl;
+//    return 0;
+//}
